788cabb55ab2c6ca46885245ca35ed269427609f
Dave Stockley <magicspon@gmail.com>
2018-11-15 22:42:40 +0000

    reset ignore

--- .docz/app/imports.js
+++
@@ -1,4 +0,0 @@
-export const imports = {
-  'src/components/Button/Button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-button-button" */ 'src/components/Button/Button.mdx'),
-}
